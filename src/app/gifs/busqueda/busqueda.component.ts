ebbd1fb8009e811c3dd0277c3b8b38d8f93f9620
LUIS-LAPTOP <luis@lamediadl.com>
2021-09-23 10:20:09 -0500

    first commit

---
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,31 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'gifs-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>
+
+  constructor(private gifsService: GifsService) { }
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value
+
+    if (!valor.trim()) {
+      this.txtBuscar.nativeElement.placeholder = 'Debes agregar un texto valido'
+      return
+    } else {
+      this.txtBuscar.nativeElement.placeholder = 'Agregar un valor'
+    }
+
+    this.gifsService.buscarGifs(valor)
+
+    this.txtBuscar.nativeElement.value = ''
+  }
+
+}
---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+
+  constructor(private gifsService: GifsService) { }
+
+  get GifsHistorial() {
+    return this.gifsService.historial
+  }
+
+}
